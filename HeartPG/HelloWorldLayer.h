a7cb22b7618d67bc89fa41b1af0fc060fa07b567
Christopher Cotton <me@christophercotton.com>
2013-01-26 10:24:45 -0600

    hellow world, yes, really

---
+++ HeartPG/HelloWorldLayer.h
@@ -0,0 +1,23 @@
+//
+//  HelloWorldLayer.h
+//  HeartPG
+//
+//  Created by Christopher Cotton on 1/26/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+
+#import <GameKit/GameKit.h>
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface HelloWorldLayer : CCLayer <GKAchievementViewControllerDelegate, GKLeaderboardViewControllerDelegate>
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
